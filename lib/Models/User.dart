b7b9170782edf05d5eebbbf3b2a4bfd592d38750
oozuser01 <daqqarayan@gmail.com>
2024-10-26 11:32:04 +0300

    MVVM

--- lib/Models/User.dart
+++ lib/Models/User.dart
@@ -4,21 +4,27 @@ class User{
     this.name="",
     this.phone="",
     this.note= "",
-});
+    this.address= "",
+
+  });
   String id;
   String name;
   String phone;
   String note;
+  String address;
   factory User. fromJson(Map<String, dynamic>json )=> User(
     id:json["id"],
     name:json["name"],
     phone:json["phone"],
     note:json["note"],
+    address:json["address"],
+
   );
     Map< String, dynamic>tojson()=>{
       "id":id,
       "name":name,
       "phone":phone,
       "note":note,
+      "address":address,
     };
 }
